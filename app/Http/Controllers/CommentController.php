f5064084ba7af771aecb7bcdbaca3487162bbcea
khk <khk0613@cside.co.kr>
2016-08-30 21:07:38 +0900

    - 댓글 삭제하기 만들기
      댓글 벨리데이션 나오게 하기,env파일 DB설정 변경

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -12,23 +12,39 @@ use App\Post;
 
 use App\Comment;
 
+use Validator;
+
 class CommentController extends Controller
 {
 	function store($post_id, Request $request)
     {
-        $this->validate($request, [
+        $post = Post::find($post_id);
+        $rules = array(
             'name' => 'required',
-            'content' => 'required'
-        ]);
+            'content' => 'required',
+            'password' => 'required'
+        );
+        $messages = array(
+            'title.required' => '타이틀을 입력해주세요',
+            'content.required' => '내용을 입력해주세요',
+            'password.required' => '비밀번호를 입력해주세요'
+        );
+        $validator = Validator::make($request->all(),$rules,$messages); 
+        if ($validator->fails()) {
+            return redirect('posts/' . $post->id)
+            ->withErrors($validator)
+            ->withInput();
+        }else{
+            
+            $comment = new Comment;
+            $comment->name = $request->name;
+            $comment->content = $request->content;
+            $comment->password = Crypt::encrypt($request->password);
+            $post->comments()->save($comment);
+            return redirect(url('posts/' . $post->id));
+        }
 
-    	$post = Post::find($post_id);
-        $comment = new Comment;
-        $comment->name = $request->name;
-        $comment->content = $request->content;
-        $comment->password = Crypt::encrypt($request->password);
-        $post->comments()->save($comment);
-
-        return redirect(url('posts/' . $post->id));
+    	
     }
     function destroy($post_id,$id,Request $request)
     {
